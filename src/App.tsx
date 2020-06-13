9bfde06ee0e6f3d0b261e08074c01e641e333b07
marci <marcianszlovak@gmail.com>
2020-06-13 18:15:31 +0200

    modify fetch url -> celsius instead of kelvin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './components/dashboard-component/dashboard.component';
-import Search from './components/search-component/search.component';
 
 function App() {
   return (
     <div className="App">
-      <Search />
       <Dashboard />
     </div>
   );
--- src/components/dashboard-component/dashboard.component.tsx
+++ src/components/dashboard-component/dashboard.component.tsx
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import Search from '../search-component/search.component';
+import Tile from '../tile-component/tile.component';
 
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Search />
+      <Tile />
+    </div>
+  );
 };
 
 export default Dashboard;
