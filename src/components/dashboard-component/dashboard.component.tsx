d4705bc9eaf5c67085675697a04fa0a712682660
marci <marcianszlovak@gmail.com>
2020-06-13 14:08:40 +0200

    setup base project

---
+++ src/components/dashboard-component/dashboard.component.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../form/form.component';
+import Tile from '../tile-component/tile.component';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Form />
+      <Tile />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/tile-component/tile.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Tile = () => {
+  return (
+    <div>
+      <p>Seattle</p>
+      <p>Washington, United States</p>
+    </div>
+  );
+};
+
+export default Tile;
