e9400d96b5a8fcf75fb23caf17db141e5bbe7529
marci <marcianszlovak@gmail.com>
2020-06-15 19:27:00 +0200

    add invalid route component

---
+++ src/components/invalid-route-component/invalid-route.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InvalidRoute: () => JSX.Element = () => {
+  return (
+    <div>
+      <p>Not a valid route, click to return to the Dashboard!</p>
+    </div>
+  );
+};
+
+export default InvalidRoute;
